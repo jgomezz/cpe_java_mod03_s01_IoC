1b5e4f2dd4f07cfaa67348b498287fc88660d0dd
jgomezz <jgomezz@gmail.com>
2026-01-11 08:41:23 -0500

    update

--- src/main/java/pe/edu/tecsup/Main.java
+++ src/main/java/pe/edu/tecsup/Main.java
@@ -2,16 +2,25 @@ package pe.edu.tecsup;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+class EmailService {
+    void sendEmail(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
+
+public class Main {
+
+    private  EmailService emailService;
+
+    Main(){
+        this.emailService = new EmailService();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        main.emailService.sendEmail("Hola mundo");
     }
 }
